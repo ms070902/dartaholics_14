5692f8c0b9533d2a77973743deb1f4d84191fdda
Mihir Shah <mihirshah070902@gmail.com>
2023-02-02 15:17:14 +0530

    backend

--- lib/state/user_info/backend/user_payload.dart
+++ lib/state/user_info/backend/user_payload.dart
@@ -1,22 +1,28 @@
 import 'dart:collection';
 import 'package:dartaholics/state/auth/constants/firebase_field_name.dart';
 import 'package:dartaholics/state/auth/typedef.dart';
-import 'package:dartaholics/state/user_info/backend/typedef.dart';
-
 import 'package:flutter/foundation.dart' show immutable;
 
 @immutable
-class UserInfoPayload extends MapView<String, String> {
+class UserInfoPayload extends MapView<String, dynamic> {
   UserInfoPayload({
     required UserId userId,
     required String? displayName,
     required String? email,
-    String? gender,
-    Map<Location, Coordinates>? location,
-    String? contact,
+    required String? gender,
+    required Map<String, String> location,
+    required String? contact,
   }) : super({
           FirebaseFieldName.userId: userId,
           FirebaseFieldName.displayName: displayName ?? '',
           FirebaseFieldName.email: email ?? '',
+          FirebaseFieldName.gender: gender ?? '',
+          FirebaseFieldName.location: {
+            for (final loc in location.entries)
+              {
+                loc.key: loc.value,
+              }
+          },
+          FirebaseFieldName.contact: contact,
         });
 }
